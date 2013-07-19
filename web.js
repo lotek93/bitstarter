7cfa38a5286407c3aeb507444cfef192a55de6db
Ubuntu <ubuntu@ip-172-31-40-193.us-west-2.compute.internal>
2013-07-19 10:57:31 +0000

    hw4p1

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var fs = require( 'fs' );
-var buf = new Buffer( 65535 );
+//var buf = new Buffer( 65535 );
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-  var buf =  fs.readFileSync( "index.html" );
-  var content = buf.toString();
+//  var buf =  fs.readFileSync( "index.html" );
+//  var content = buf.toString();
+  var content =  fs.readFileSync( "index.html" ).toString();
   response.send( content );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
